b2b76804d55fd13109123c49f1184b4102116d74
Gustavo Mendes <gusttavo.me@outlook.com>
2026-08-23 12:58:00 -0300

    NetBpfLoad: Relax 5.15 kernel requirement
    Change-Id: I6b51c0ac8b177a24b8db7a5bf9366be1c2ddc041
    Signed-off-by: Gustavo Mendes <gusttavo.me@outlook.com>

--- bpf/loader/NetBpfLoad.cpp
+++ bpf/loader/NetBpfLoad.cpp
@@ -1577,8 +1577,7 @@ static int doLoad(char** argv, char * const envp[]) {
 
     // 26Q4 bumps the kernel requirement up to 5.15
     if (isAtLeast26Q4 && !isAtLeastKernelVersion(5, 15)) {
-        ALOGE("Android 26Q4 requires kernel 5.15.");
-        return 7;
+        ALOGW("Android 26Q4 requires kernel 5.15.");
     }
 
     // Technically already required by U, but only enforce on V+
